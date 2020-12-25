df934235d687db79247cfae9e21b79d247463287
taegukusa <taegukusa@gmail.com>
2020-12-25 10:05:49 +0800

    Voltage threshold: 3.3v

--- nrf/applications/asset_tracker_blockchain/src/modem/modem_helper.c
+++ nrf/applications/asset_tracker_blockchain/src/modem/modem_helper.c
@@ -173,9 +173,9 @@ void CheckPower(void)
 {
     volatile float adc_voltage = 0;
     adc_voltage = iotex_modem_get_battery_voltage();
-    if(adc_voltage < 3.0)
+    if(adc_voltage < 3.3)
     {
-        printk("power lower than 2.7 \n");
+        printk("power lower than 3.3 \n");
         PowerOffIndicator();
     }
 }
