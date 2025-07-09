b82888ede2e8ebc9e04dba37b9ad980339120194
Jianian.Zhang <46586581+iloveyou-github@users.noreply.github.com>
2025-07-09 22:37:42 +0800

    Update ver.h

--- nrf/applications/ioID/src/ver.h
+++ nrf/applications/ioID/src/ver.h
@@ -10,7 +10,7 @@
 
 #define  IOTEX_APP_NAME  "Riverrock"
 
-#define  RELEASE_VERSION    "2.1.0"
+#define  RELEASE_VERSION    "2.1.1"
 
 /*  application version */
 #define IOTEX_APP_VERSION IOTEX_APP_NAME" "RELEASE_VERSION
